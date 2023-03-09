a24ceccc4b09bc2de56eeef019382c2c0cf29efa
ezequiel <ps3ezequiel@gmail.com>
2023-03-09 20:13:48 +0100

    refactor working 1.0.0

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -4,13 +4,22 @@ import { limitProducts } from '../logic/middleware.js'
 
 export const apiRouter = Router()
 
-const regex = /^.products\?(page|limit)=.+/mgi
+apiRouter
+  .route('/products/:id')
+  .get(async (req, res) => {
+    const product = await PM.getProductById(req.params.id)
+    if (product.error) res.json({ error: product.msg })
+    else {
+      res.json(product)
+    }
+  })
 
 apiRouter
-  .route(regex)
-  .get(async (req, res) => {
+  .route('/products')
+  .get(async (req, res, next) => {
+    const { limit, page } = req.query
+    if (limit === undefined && page === undefined) return next()
     try {
-      const { limit, page } = req.query
       const allProducts = await PM.getProducts()
       const arr = await limitProducts(allProducts, limit, page)
       res.json({
@@ -24,19 +33,6 @@ apiRouter
       })
     }
   })
-
-apiRouter
-  .route('/products/:id')
-  .get(async (req, res) => {
-    const product = await PM.getProductById(req.params.id)
-    if (product.error) res.json({ error: product.msg })
-    else {
-      res.json(product)
-    }
-  })
-
-apiRouter
-  .route('/products')
   .get(async (req, res) => {
     const products = await PM.getProducts()
     res.json({
