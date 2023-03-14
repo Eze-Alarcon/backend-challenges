ef19ee5d24fb201b66f7b991226766c95570fcaa
ezequiel <ps3ezequiel@gmail.com>
2023-03-14 19:32:31 +0100

    first step

--- src/app.js
+++ src/app.js
@@ -3,14 +3,37 @@
 import express from 'express'
 import { apiRouter } from './routers/apiRouter.js'
 import { cartRouter } from './routers/cartRouter.js'
+import { homeRouter } from './routers/homeRouter.js'
 import { ERRORS } from './mocks/messages.js'
+import { engine } from 'express-handlebars'
 
 const PORT = 8080
 
 const app = express()
+app.use('/static', express.static('./static'))
+
+/*
+  TO DO: Agregar el urlEncoded!
+  * Esto sera para el formulario
+*/
+
+app.engine('handlebars', engine())
+app.set('views', './views')
 
 app.use('/api/products', apiRouter)
 app.use('/api/cart', cartRouter)
+app.use(homeRouter)
+
+// TO DO with web sockets
+
+app.get('/realtimeproducts', async (req, res, next) => {
+  try {
+    res.render('home.handlebars', { titulo: 'Inicio', encabezado: 'Inicio' })
+  } catch (error) {
+    console.log(error)
+    next(error.message)
+  }
+})
 
 app.use((error, req, res, next) => {
   try {
