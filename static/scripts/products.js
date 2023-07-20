8809ae131881f5ec967728354e8f33b93197d049
ezequiel <ps3ezequiel@gmail.com>
2023-07-20 23:21:49 +0200

    feat(deleteproduct): admins can delete products

--- static/scripts/products.js
+++ static/scripts/products.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 const link = document.getElementById('linkToCart') ?? null
 let cartID = null
 
 if (link !== null) cartID = link.dataset.cart
 
-// eslint-disable-next-line no-unused-vars
 async function addProductToCart (productID) {
   const FETCH_URL = `http://localhost:8080/api/carts/${cartID}/product/${productID}`
   await fetch(FETCH_URL, { method: 'PUT' })
+}
+
+async function deleteProduct (productID) {
+  const FETCH_URL = `http://localhost:8080/api/products/${productID}`
+  await fetch(FETCH_URL, { method: 'DELETE' })
+  window.location.reload()
 }
